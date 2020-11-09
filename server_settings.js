0f2661b3937f9d751e52610387de4effd9aebde4
Lasse Skaalum <lskaal19@student.aau.dk>
2020-11-09 21:55:08 +0100

    Removed sql-conf from server-settings, as it isn't used

--- server_settings.js
+++ server_settings.js
@@ -2,11 +2,6 @@ const settings = {
   log: true,
   logErrors: true,
   production: false,
-  sqlConf: {
-    user: `admin`,
-    password: `BaX24w7sgyfxWr`,
-    database: `biler`,
-  },
   port: 3000,
 };
 
